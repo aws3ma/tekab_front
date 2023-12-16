9c370175b6d198094dd0344d773beacdf3611a0b
Oussema Trabelsi <oussema99trabelsi@gmail.com>
2023-12-16 19:11:47 +0100

    crud working

---
+++ src/models/Person.ts
@@ -0,0 +1,11 @@
+class Person {
+    id: string;
+    fullName: string;
+    age : number;
+    constructor(id:string,fullName:string,age:number){
+        this.id=id;
+        this.fullName=fullName;
+        this.age=age;
+    }
+}
+export default Person
