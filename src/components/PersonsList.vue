2fd4357d8e71c7af2c37eba54e8a2c3063efd037
Oussema Trabelsi <oussema99trabelsi@gmail.com>
2023-12-23 09:53:30 +0100

    add email

--- src/components/PersonsList.vue
+++ src/components/PersonsList.vue
@@ -1,44 +1,48 @@
 <template>
     <div class="m-4">
         <form>
-
             <div class="flex flex-row items-center gap-2">
                 <label for="name" class="text-lg">Name</label>
-                <input id="name" v-model="v$.form.fullName.$model" type="text" name="name" class="rounded-md" placeholder="Full name">
+                <input id="name" v-model="v$.form.full_name.$model" type="text" name="name" class="rounded-md"
+                    placeholder="Full name">
                 <label for="age" class="text-lg">Age</label>
                 <input id="age" v-model="form.age" type="number" name="age" min="0" class="rounded-md">
-                <!-- <input type="submit" value="Add"> -->
+                <label for="email" class="text-lg">Email</label>
+                <input id="email" v-model="v$.form.email.$model" type="email" name="email" min="0" class="rounded-md">
                 <button
                     class="flex p-2.5 bg-green-500 rounded-xl hover:rounded-3xl hover:bg-green-600 transition-all duration-300 text-white"
-                    v-on:click="addPerson()" type="button" v-if="!edit" >
+                    v-on:click="addPerson()" type="button" v-if="!edit">
                     <img src="../assets/img/add.png" alt="" srcset="" class="h-6 w-6">
-
                 </button>
                 <button
                     class="flex p-2.5 bg-yellow-500 rounded-xl hover:rounded-3xl hover:bg-yellow-600 transition-all duration-300 text-white"
                     v-on:click="editPerson('')" type="button" v-if="edit">
                     <img src="../assets/img/edit.png" alt="" srcset="" class="h-6 w-6">
-
                 </button>
             </div>
-            <div>
-                <ul>
-                    <li v-for="(error, index) of v$.form.fullName.$errors" :key="index">
-                        <p class="text-red-500">{{ error.$message }}</p>
+            <div class="pl-4">
+                <ul class="list-disc">
+                    <li class="text-red-500" v-for="(error, index) of v$.form.full_name.$errors" :key="index">
+                        {{ error.$message }}
+                    </li>
+                    <li class="text-red-500" v-for="(error, index) of v$.form.email.$errors" :key="index">
+                        {{ error.$message }}
                     </li>
                 </ul>
             </div>
         </form>
-
         <table class="w-2/5 text-md text-left mt-4">
             <tr>
                 <th>Full name</th>
                 <th>Age</th>
+                <th>Email</th>
                 <th>Action</th>
             </tr>
             <tr v-for="person in personList" :key="person.id">
-                <td>{{ person.fullName }}</td>
+                <td>{{ person.full_name }}</td>
                 <td>{{ person.age }}</td>
+                <td v-if="person.email">{{ person.email }}</td>
+                <td v-if="!person.email">N/A</td>
                 <td>
                     <div class="flex flex-row gap-1">
                         <button
@@ -62,10 +66,10 @@ import axios from "axios";
 import { defineComponent } from "vue"
 import Person from "@/models/Person";
 import useVuelidate from '@vuelidate/core'
-import { required, minLength } from '@vuelidate/validators'
+import { required, minLength, email } from '@vuelidate/validators'
 export function validName(name: string) {
     let validNamePattern = new RegExp("^[a-zA-Z]+(?:['\\s][a-zA-Z]+)*$");
-    if(name == '')
+    if (name == '')
         return false;
     if (validNamePattern.test(name)) {
         return true;
@@ -85,57 +89,54 @@ export default defineComponent({
             edit: false,
             form: {
                 id: '',
-                fullName: '',
-                age: 0
+                full_name: '',
+                age: 0,
+                email: ''
             }
         };
     },
     methods: {
         async getPersonList() {
             const data = await axios.get("https://entretien.oussema99trabelsi.workers.dev/api/users");
-            data.data.forEach((element: any) => {
-                const p = new Person(element.id, element.full_name, element.age)
+            data.data.forEach((element: Person) => {
+                const p = new Person(element.id, element.full_name, element.age, element.email)
                 this.personList.push(p)
             });
-            // this.personList = data.data;
         },
         async addPerson() {
             if (this.v$.$error) {
                 return
             }
-            const data = await axios.post("https://entretien.oussema99trabelsi.workers.dev/api/user", { 'fullName': this.form.fullName, 'age': this.form.age })
-            const p = new Person(data.data[0].id, data.data[0].full_name, data.data[0].age)
-            this.personList.push(p)
+            if (!(this.form.full_name && this.form.email)) return
+            const data: Person = (await axios.post("https://entretien.oussema99trabelsi.workers.dev/api/user", { full_name: this.form.full_name, age: this.form.age, email: this.form.email })).data[0]
+            this.personList.push(data)
             this.form.id = ''
             this.form.age = 0
-            this.form.fullName = ''
+            this.form.full_name = ''
+            this.form.email = ''
         },
         async editPerson(id: string) {
             if (id === '') {
                 if (this.v$.$anyDirty)
                     return
-                const data = await axios.put("https://entretien.oussema99trabelsi.workers.dev/api/user", { 'fullName': this.form.fullName, 'age': this.form.age, 'id': this.form.id })
+                const data: Person = (await axios.put("https://entretien.oussema99trabelsi.workers.dev/api/user", { 'fullName': this.form.full_name, 'age': this.form.age, 'id': this.form.id })).data[0]
                 const index = this.personList.findIndex((obj: Person) => obj.id === this.form.id);
-                const p = new Person(data.data[0].id, data.data[0].full_name, data.data[0].age)
-                this.personList[index] = p;
+                this.personList[index] = data;
                 this.edit = false
                 this.form.age = 0
-                this.form.fullName = ''
+                this.form.full_name = ''
                 this.form.id = ''
             }
             else {
                 const toEdit = this.personList.find((obj: Person) => obj.id === id)
                 if (toEdit) {
-                    this.form.id = toEdit.id
-                    this.form.fullName = toEdit.fullName
-                    this.form.age = toEdit.age
+                    this.form = toEdit
                     this.edit = true
                 }
             }
         },
         async deletePerson(id: string) {
             const index = this.personList.findIndex(obj => obj.id === id);
-
             if (index !== -1) {
                 this.personList.splice(index, 1); // Removes one element at the found index
             }
@@ -145,16 +146,24 @@ export default defineComponent({
     validations() {
         return {
             form: {
-
-                fullName: {
+                full_name: {
                     required,
                     $autoDirty: true,
                     minLength: minLength(2),
                     name_validation: {
                         $validator: validName,
-                        $message: 'Invalid Name. Valid name only contain letters and spaces'
+                        $message: 'Invalid Name.'
                     }
                 },
+                email: {
+                    required,
+
+                    email,
+                    // name_validation: {
+                    //     $validator: email,
+                    //     $message: 'Invalid email.'
+                    // }
+                }
             }
         }
     },
--- src/models/Person.ts
+++ src/models/Person.ts
@@ -1,11 +1,13 @@
 class Person {
-    id: string;
-    fullName: string;
-    age : number;
-    constructor(id:string,fullName:string,age:number){
-        this.id=id;
-        this.fullName=fullName;
-        this.age=age;
-    }
+  id: string;
+  full_name: string;
+  age: number;
+  email: string;
+  constructor(id: string, fullName: string, age: number, email: string) {
+    this.id = id;
+    this.full_name = fullName;
+    this.age = age;
+    this.email = email;
+  }
 }
-export default Person
+export default Person;
